ff2514f36e154181d560d9b3432777b382ebce08
rubaal <67191428+rubaal@users.noreply.github.com>
2022-10-06 14:34:24 +0300

    Add files via upload

---
+++ day 4.kt
@@ -0,0 +1,33 @@
+package com.example.day4
+
+fun main(){
+   cal(4.0,6.0,)
+    cal(4.0,6.0,"*")
+    cal(4.0,6.0,"/")
+   cal(4.0,6.0,"-")
+
+    val result = addition(2, 7)
+    reverse(result)
+}
+
+fun addition(num1: Int, num2: Int): Boolean {
+    return num1 > num2
+}
+
+fun reverse(a: Boolean) {
+    val b = !a
+    println(b)
+}
+
+fun cal (number1 :Double , number2 :Double , name :String= "+") {
+when (name){
+    "+"-> println ("add: ${number1 + number2}")
+    "-"-> println("sub: ${number1 - number2}")
+    "*"-> println("mul: ${number1 * number2}")
+    "/"-> println("dev: ${number1 / number2}")
+    else-> println("nothing")
+}
+}
+
+
+
